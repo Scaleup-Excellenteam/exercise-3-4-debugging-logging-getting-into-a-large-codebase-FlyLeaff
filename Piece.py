2b5aea1323511b6505794f69f0491b4ce8eb0760
FlyLeaff <kohav24@gmail.com>
2024-05-21 18:39:31 +0300

    Fixed Knight issues:
    - knight valid piece takes loop went till 6 rather than 8
    - added in bounds checks

--- Piece.py
+++ Piece.py
@@ -163,13 +163,14 @@ class Knight(Piece):
         row_change = [-2, -2, -1, -1, +1, +1, +2, +2]
         col_change = [-1, +1, -2, +2, -2, +2, +1, -1]
 
-        for i in range(0, 6):
+        for i in range(8):       # FIX 
             new_row = self.get_row_number() + row_change[i]
             new_col = self.get_col_number() + col_change[i]
-            evaluating_square = game_state.get_piece(new_row, new_col)
-            # when the square with new_row and new_col contains a valid piece and the player is different
-            if game_state.is_valid_piece(new_row, new_col) and self.get_player() is not evaluating_square.get_player():
-                _moves.append((new_row, new_col))
+            if 0 <= new_row < 8 and 0 <= new_col < 8:   # FIX added checks to make sure col and row are in bounds
+                evaluating_square = game_state.get_piece(new_row, new_col)
+                # when the square with new_row and new_col contains a valid piece and the player is different
+                if game_state.is_valid_piece(new_row, new_col) and self.get_player() is not evaluating_square.get_player():
+                    _moves.append((new_row, new_col))
         return _moves
 
     def get_valid_piece_moves(self, game_state):
